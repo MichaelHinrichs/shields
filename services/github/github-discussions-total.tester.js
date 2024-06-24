c7456abb8a5340b2240437fedda0e736972447dd
NintenHero <37460517+MichaelHinrichs@users.noreply.github.com>
2024-06-24 14:06:23 -0500

    Update github-discussions-total.tester.js

--- services/github/github-discussions-total.tester.js
+++ services/github/github-discussions-total.tester.js
@@ -4,11 +4,11 @@ export const t = await createServiceTester()
 
 t.create('GitHub Total Discussions (repo not found)')
   .get('/not-a-user/not-a-repo.json')
-  .expectBadge({ label: 'discussions', message: 'repo not found' })
+  .expectBadge({ label: 'discussions', message: 'repo not found', namedLogo: 'githubdiscussion' })
 
 // example: 6000 total
 const numberSpaceTotal = withRegex(/^\d+ total$/)
 
 t.create('GitHub Total Discussions (repo having discussions)')
   .get('/vercel/next.js.json')
-  .expectBadge({ label: 'discussions', message: numberSpaceTotal })
+  .expectBadge({ label: 'discussions', message: numberSpaceTotal, namedLogo: 'githubdiscussion' })
